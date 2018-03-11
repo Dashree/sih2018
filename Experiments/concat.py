7014d62ce2808b4722569703cf34c0041cd942c1
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-11 22:13:29 +0530

    creating a video file from single clip and from concatenated clips

---
+++ Experiments/concat.py
@@ -0,0 +1,6 @@
+import moviepy.editor as moviepy
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+clip1 = moviepy.ImageSequenceClip('image', fps = 1)
+clip2 = moviepy.ImageSequenceClip('image1', fps = 1)
+final_clip = concatenate_videoclips([clip1,clip2])
+final_clip.write_videofile("concat.webm", fps=44,codec='libvpx-vp9')
