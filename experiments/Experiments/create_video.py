2853fa65e85f01ea7f057d36d7a4c9915e9246d4
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-14 16:46:44 +0530

    creating video with single image

--- experiments/Experiments/create_video.py
+++ experiments/Experiments/create_video.py
@@ -1,9 +1,8 @@
 """
-Height is provided from the terminal and can be 360, 720, 1080.
-Width is automatically calculated as per height. Doesn't accept externally provided width
+Creating video with a single image
 """
 import moviepy.editor as moviepy
 import sys
 height = sys.argv[1]
-clip = moviepy.ImageSequenceClip('images', fps = 1)
-clip.resize(height=int(height)).write_videofile("video1fps.webm", fps=22,codec='libvpx-vp9')
+clip = moviepy.ImageClip('images/3DIMG_01SEP2017_0030_L1C_ASIA_MER_IR1.jpg', duration=1)
+clip.resize(height=int(height)).write_videofile("singleVideo.webm", fps=1,codec='libvpx-vp9')
