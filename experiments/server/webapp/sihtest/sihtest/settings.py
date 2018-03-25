7eec191a51e68b93ce9b76a583b69016839f29ea
Samruddhi Lahoti <samruddhi.lahoti@cumminscollege.in>
2018-03-25 13:09:15 +0530

    trials for video playing

--- experiments/server/webapp/sihtest/sihtest/settings.py
+++ experiments/server/webapp/sihtest/sihtest/settings.py
@@ -120,7 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+print (STATIC_ROOT)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
