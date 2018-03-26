ab680874dfde84c99396c94f86f191cda59a1966
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-26 20:16:32 +0530

    working concat code

--- experiments/server/webapp/sihtest/UserInterface/models.py
+++ experiments/server/webapp/sihtest/UserInterface/models.py
@@ -1,30 +1,6 @@
 from django.db import models
 import os
 from datetime import date
-
-"""
-# Create your models here.
-
-# def upload_path_videos(instance, filename):
-#     directory =os.path.dirname('videos/{date}'%(instance.date))
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-#     directory =os.path.dirname('videos/{date}/{res}'%(instance.date, instance.res))
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-#     directory =os.path.dirname('videos/{date}/{res}/{fps}'%(instance.date, instance.resfield, instance.fpsfield))
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-#     return "videos/{date}/{res}/{fps}/{file}"%(instance.date, instance.resfield, instance.fpsfield, filename)
-
-
-# def upload_path_images(instance, filename):
-#     directory =os.path.dirname('/mnt/c/sih2018/experiments/worker/images/{date}'%(instance.date))
-#     if not os.path.exists(directory):
-#         os.makedirs(directory)
-#     return "/mnt/c/sih2018/experiments/worker/images/{date}/{file}"%(instance.date, filename)
-"""
-
 class videoDir(models.Model):
     res = (
        (0, 360),
@@ -43,11 +19,6 @@ class videoDir(models.Model):
     upload = models.TextField()    # to upload it to a folder
     date = models.DateField()   # passing a reference to the EntryDate
 
-##    class Meta:
-##        unique_together = ('fpsfield', 'resfield', 'date',)
-
-    def __str__(self):
-        return str(self.upload)
 
 
 class upload_image(models.Model):
