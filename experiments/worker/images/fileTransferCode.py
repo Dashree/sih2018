7b94bb3b7e92e91385dcb9432e65c54921395ddf
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-27 20:26:28 +0530

    changed logic for file open.

--- experiments/worker/images/fileTransferCode.py
+++ experiments/worker/images/fileTransferCode.py
@@ -3,9 +3,9 @@ import shutil
 import os, sys, glob
 import time
 
-path = "/mnt/c/sih2018/Hackathon_Images"
+path = "/mnt/c/Hackathon/Hackathon_Images"
 source = os.listdir(path)
-destination = "/mnt/c/sih2018/sih2018/experiments/worker/images"
+destination = "/mnt/c/images"
 for files in source:
 	if files.endswith(".jpg" or ".jpeg" or ".png"):
 		shutil.copy(os.path.join(path, files),destination)
