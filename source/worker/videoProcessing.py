9c3aa39d50a350577545e56e6561b9812ecd759d
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-30 11:25:27 +0530

    copy Image to the folder

--- source/worker/videoProcessing.py
+++ source/worker/videoProcessing.py
@@ -1,5 +1,6 @@
-import subprocess, time, os, sys
+import os, shutil
 from datetime import datetime
+from django.conf import settings
 
 class videoProcessing():
     def __init__(self, imageSrc):
@@ -12,10 +13,22 @@ class videoProcessing():
         imageName = os.path.basename(self.imageSrc)
         list = imageName.split("_")
         self.dateStr = str(list[1]);
+        print(str(list[1]));
         conv = datetime.strptime(self.dateStr, '%d%b%Y')
         self.date = conv.date().strftime('%d-%m-%Y')
-        return(self.date)
     
+    
+    def copyImage(self):
+        self.dateImagePath = 'media/' + self.date + '/images'
+        print(self.dateImagePath)
+        self.destImagePath = os.path.join(settings.BASE_DIR, os.path.join(self.dateImagePath, os.path.basename(self.imageSrc)))
+        directory = os.path.dirname(self.destImagePath)
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        shutil.copy(self.imageSrc, self.destImagePath)
+        
+
     
 vP = videoProcessing('/mnt/c/Hackathon_Images/3DIMG_01SEP2017_0000_L1C_ASIA_MER_IR1.jpg')
 vP.getDate()
+vP.copyImage()
