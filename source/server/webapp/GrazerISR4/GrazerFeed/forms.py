3372a24d74a3fa82ca96a3cffd92cb796a9edc97
Samruddhi Lahoti <samruddhi.lahoti@cumminscollege.in>
2018-03-30 18:17:28 +0530

    playing video with start & end time

--- source/server/webapp/GrazerISR4/GrazerFeed/forms.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/forms.py
@@ -1,14 +1,26 @@
 from django import forms
 import datetime
+from django.http import HttpResponse
+from django.core import validators
 
 class OptionsPage(forms.Form):
     fromdate = forms.DateField()
+    print(fromdate)
     todate = forms.DateField()
     fromtime = forms.TimeField()
     totime = forms.TimeField()
     res = forms.IntegerField()
     fps = forms.IntegerField()
 
+    def check_date(self):
+        from_date = self.cleaned_data['fromdate']
+        to_date = self.cleaned_data['todate']
+        if from_date > datetime.date.today() or to_date > datetime.date.today() or to_date < from_date:
+            raise forms.ValidationError("Invalid date pair")
+        else:
+            list_date = [from_date, to_date]
+            return list_date
+
 class LoginPage(forms.Form):
-    User = forms.CharField()
+    Username = forms.CharField()
     Password = forms.CharField()
--- source/server/webapp/GrazerISR4/GrazerISR4/urls.py
+++ source/server/webapp/GrazerISR4/GrazerISR4/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/',  admin.site.urls),
     path('GrazerFeed/', include('GrazerFeed.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
