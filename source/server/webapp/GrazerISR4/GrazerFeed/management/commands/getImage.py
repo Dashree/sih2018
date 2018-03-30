b6e06fd9975fb05c4c80ca07be828ec8168925fe
ShreedaSG <ShreedaSG@localhost>
2018-03-30 07:10:46 +0530

    added code from triggering a event

--- source/server/webapp/GrazerISR4/GrazerFeed/management/commands/getImage.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/management/commands/getImage.py
@@ -4,3 +4,23 @@ from watchdog.events import PatternMatchingEventHandler
 from django.core.management.base import BaseCommand
 
 from .videoProcessing import VideoProcessing
+
+class Handler(PatternMatchingEventHandler):
+    def __init__(self):
+        super(Handler, self).__init__(patterns=['*.jpg', '*.png'],
+            ignore_directories=True, case_sensitive=False)
+
+    def on_created(self,event):
+        videoP = VideoProcessing(event.src_path)
+        videoP.createVideo()
+
+
+
+class Command(BaseCommand):
+    def handle(self, **options):
+        observer = Observer()
+        event_handler = Handler()
+        observer.schedule(event_handler,'/mnt/c/images', recursive=True)
+        observer.start()
+        observer.join()
+
