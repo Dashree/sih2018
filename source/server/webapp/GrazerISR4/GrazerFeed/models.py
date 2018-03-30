cdbe05689d169cd0d93d8a958ae382f5454bd717
Akshata Jahagirdar <akshata.jahagirdar@cumminscollege.in>
2018-03-30 12:35:29 +0530

    created database models for images and videos

--- source/server/webapp/GrazerISR4/GrazerFeed/models.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/models.py
@@ -1,7 +1,17 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from datetime import date
 # Create your models here.
 
 class User(AbstractUser):
     port = models.IntegerField(max_length = 100, null = False, unique  = True)
     
+class videoDir(models.Model):
+    resfield = models.IntegerField(null = False)
+    fpsfield = models.IntegerField(null = False)
+    uploadPath = models.TextField()
+    uploadDate = models.DateField()
+    
+class ImageDir(models.Model):
+    upload = models.TextField()
+    date = models.DateField()
