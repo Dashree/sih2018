f877f85492d076162275abcba76ec77cec06e6c6
Samruddhi Lahoti <samruddhi.lahoti@cumminscollege.in>
2018-03-30 14:45:03 +0530

    linked the three html pages

--- source/server/webapp/GrazerISR4/GrazerFeed/forms.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/forms.py
@@ -9,12 +9,6 @@ class OptionsPage(forms.Form):
     res = forms.IntegerField()
     fps = forms.IntegerField()
 
-    def check_date(self):
-        from_date = self.cleaned_data['fromdate']
-        to_date = self.cleaned_data['todate']
-
-        if from_date > datetime.date.today() or to_date > datetime.date.today() or to_date < from_date:
-            list_date = None
-        else:
-            list_date = [from_date, to_date]
-        return list_date
+class LoginPage(forms.Form):
+    User = forms.CharField()
+    Password = forms.CharField()
--- source/server/webapp/GrazerISR4/GrazerFeed/views.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/views.py
@@ -1,22 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-from django.contrib import messages
-from .forms import OptionsPage#, LoginForm
+#from django.contrib import messages
+from .forms import OptionsPage, LoginPage
 
 def index(request):
     return HttpResponse("Hello, world. You're at the GrzerFeed index.")
 
 def option(request):
-    '''if request.method == 'POST':
+    if request.method == 'POST':
         form = OptionsPage(request.POST)
         if form.is_valid():
-            list_date = form.check_date()
-            if list_date == None:
-                messages.error(request, 'Invalid from and to date pair')
-                return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
-            fromdate = list_date[0]
-            todate = list_date[1]
-    else:'''
-    print ('in option')
-    return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
+            from_time = form.cleaned_data['fromtime']
+            to_time = form.cleaned_data['totime']
+            from_date = form.cleaned_data['fromdate']
+            to_date = form.cleaned_data['todate']
+            Res = form.cleaned_data['res']
+            FPS = form.cleaned_data['fps']
+            return render(request, 'GrazerFeed/VideoPage.html')
+        else:
+            return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
+    else:
+        return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
+
+def loginuser(request):
+    if request.method == 'POST':
+        form = LoginPage(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['Username']
+            password = form.cleaned_data['Password']
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
+    else:
+        return render(request, 'GrazerFeed/LoginPage.html', { 'form': LoginPage()})        
