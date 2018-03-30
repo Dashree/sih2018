485eed02be06bbf1ecef5bc6dd2b7c26f6e7028b
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-30 12:17:36 +0530

    added file transfer code

---
+++ source/worker/fileTransferCode.py
@@ -0,0 +1,12 @@
+
+import shutil
+import os, sys, glob
+import time
+
+path = "/mnt/c/Hackathon/Hackathon_Images"
+source = os.listdir(path)
+destination = "/mnt/c/images"
+for files in source:
+	if files.endswith(".jpg" or ".jpeg" or ".png"):
+		shutil.copy(os.path.join(path, files),destination)
+	time.sleep(60)
