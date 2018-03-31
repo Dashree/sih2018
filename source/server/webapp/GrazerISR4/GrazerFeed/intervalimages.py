d9ddd0ebae1d702c48ea6be58f8e0af4255e2e16
Samruddhi Lahoti <samruddhi.lahoti@cumminscollege.in>
2018-03-31 14:31:08 +0530

    code errors removed

--- source/server/webapp/GrazerISR4/GrazerFeed/intervalimages.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/intervalimages.py
@@ -7,11 +7,11 @@ from django.conf import settings
 from .management.commands.videoProcessing import VideoProcessing
 
 def interval(source,dur,res):
-    tempfile.mkdtemp(prefix = Grz)
+    tdir = tempfile.mkdtemp(prefix = "Grz")
     for files in source:
-        shutil.copy(files,tempfile.gettempdir())
+        shutil.copy(files,tdir)
 
-    clip = moviepy.ImageSequenceClip('temp', fps = dur)
+    clip = moviepy.ImageSequenceClip(tdir, fps = dur)
     clip.resize(newsize=(VideoProcessing.getMatchingWidth(res),res)).write_videofile("output.webm", fps=len(source),codec='libvpx-vp9')
 
     outputpath = os,path.join(settings.BASE_DIR, 'media', 'output_interval')
