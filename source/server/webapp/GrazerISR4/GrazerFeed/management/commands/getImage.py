384c572a4b2b84dcbf44d7bffa3442858f51d75f
ShreedaSG <ShreedaSG@localhost>
2018-03-31 10:37:57 +0530

    blending code integrated

--- source/server/webapp/GrazerISR4/GrazerFeed/management/commands/getImage.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/management/commands/getImage.py
@@ -1,10 +1,59 @@
+from datetime import datetime,timedelta
+import os
+import os.path
+
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
 
 from django.core.management.base import BaseCommand
 from GrazerFeed.models import ImageUpload, VideoUpload
 
+from PIL import Image, ImageChops
 from .videoProcessing import VideoProcessing
+
+def getImageTime(srcpath):
+    imageName = os.path.basename(srcpath)
+    list1 = imageName.split("_")
+    imgDate = datetime.strptime(list1[1], '%d%b%Y').date()
+    imgTime = datetime.strptime(list1[2], '%H%M').time()
+    
+    return datetime.combine(imgDate,imgTime)
+
+def calc_intermediate(startimg, starttime, endimage): 
+    def time_range(start, end, step):
+        s = start
+        while (s < end):
+            yield s
+            s = s + step
+            
+    if startimg == None:
+        yield endimage
+        raise StopIteration
+
+    
+    endtime = getImageTime(endimage)
+    timediff = endtime - starttime
+    tdiff = timedelta(minutes=45)
+    if timediff > tdiff :
+        img1 = Image.open(startimg)
+        img2 = Image.open(endimage)
+
+        t_step = timedelta(minutes=30)
+        rangeAlpha = 10
+
+        basedir = os.path.dirname(endimage)
+        for t in time_range(starttime, endtime, t_step):
+            alpha = (t-starttime)/(endtime-starttime)
+            imgBlend = ImageChops.blend(img1,img2,alpha)
+            imgname = t.strftime("3DIMG_%d%b%Y_%H%M_L1C_ASIA_MER_IR1.JPG")
+            imgname = os.path.join(basedir, imgname)
+            imgBlend.save(imgname)
+            print("Intermediate image : %s" % imgname)
+            yield imgname
+    else:
+        yield endimage
+        
+    
 
 class Handler(PatternMatchingEventHandler):
     def __init__(self):
@@ -23,8 +72,23 @@ class Handler(PatternMatchingEventHandler):
             newVideo.uploadPath = videopath
             newVideo.save()
 
+    def convert_image_with_intermediate(self, newimagepath):
+        try:
+            last_img = ImageUpload.objects.latest('imgDate','imgTime')
+            lastdt = datetime.combine(last_img.imgDate,last_img.imgTime)
+            last_img= last_img.upload
+        except Exception as exp:
+            import pdb
+            pdb.set_trace()
+    
+            last_video = None
+            lastdt = None
+
+        for path in calc_intermediate(last_img, lastdt, newimagepath):
+            self.convert_image(path)
+        
     def on_created(self,event):
-        self.convert_image(event.src_path)
+        self.convert_image_with_intermediate(event.src_path)
         
 class Command(BaseCommand):
     def start_observer(self):
@@ -35,6 +99,6 @@ class Command(BaseCommand):
         observer.join()
 
     def handle(self, **options):
-        self.start_observer()
-        #handler = Handler()
-        #handler.convert_image('/mnt/c/Hackathon_Images/3DIMG_01SEP2017_0000_L1C_ASIA_MER_IR1.jpg')
+        #self.start_observer()
+        handler = Handler()
+        handler.convert_image_with_intermediate('/mnt/c/images/3DIMG_01SEP2017_1130_L1C_ASIA_MER_IR1.jpg')
