380358faeef91a132a6b82057fd49e8593772ae6
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-31 07:05:16 +0530

    with Demuxer logic

--- source/server/webapp/GrazerISR4/GrazerFeed/views.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/views.py
@@ -6,7 +6,7 @@ from .forms import OptionsPage, LoginPage
 from django.contrib.auth import authenticate, login
 from django.conf import settings
 from .calculate import calculate
-from .management.commands.videoProcessing import VideoProcessing
+from .management.commands.videoProcessing import VideoProcessing, Demuxer
 from .models import VideoUpload
 
 def index(request):
@@ -25,10 +25,12 @@ def option(request):
             day = to_date - from_date
             pathlist = []
             pathlist = VideoUpload.objects.filter(uploadDate__range=[from_date, to_date])
+            pathlist = [path.uploadPath for path in pathlist]
+            #pathlist.append(pathlist.uploadpath)
             sec = calculate(day.days, from_time.hour, from_time.minute, to_time.hour, to_time.minute, Fps)
             #videopath = settings.MEDIA_URL + str(from_date) + '/videos/video_' + str(Res) + '_' + str(Fps) + '.webm'
-            videopath = VideoProcessing()
-            return render(request, 'GrazerFeed/VideoPage.html', context={'videopath' :  videopath.multipleDemuxInput(pathlist, Res, Fps), 'start' : sec[0], 'end' : sec[1]})
+            videopath = Demuxer(pathlist, Res, Fps)
+            return render(request, 'GrazerFeed/VideoPage.html', context={'videopath' :  videopath.multipleDemuxInput(), 'start' : sec[0], 'end' : sec[1]})
         else:
             print('form is not valid')
             return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()}, {'error_message' : 'Invalid field values'})
