2634a74950b7c9f4df6fa5ee176980354e2dc8af
lavina agarwal <lavina.agarwal@cumminscollege.in>
2018-03-31 17:19:04 +0530

    errors fixed.

--- source/server/webapp/GrazerISR4/GrazerFeed/views.py
+++ source/server/webapp/GrazerISR4/GrazerFeed/views.py
@@ -1,20 +1,48 @@
+import os
+from datetime import datetime, timedelta
+
+#from django.contrib import messages
+from django.contrib.auth import authenticate, login
+from django.conf import settings
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.shortcuts import redirect
-#from django.contrib import messages
+
 from .forms import OptionsPage, LoginPage
-from django.contrib.auth import authenticate, login
-from django.conf import settings
 from .calculate import calculate
 from .management.commands.videoProcessing import VideoProcessing, Demuxer
-from .models import VideoUpload
-import os
+from .models import VideoUpload, ImageUpload
+from .intervalimages import interval
+    
+
+def sendimage(from_date, to_date, from_time, to_time, dur, Fps, Res):
+    start_time  = datetime.combine(from_date, from_time)
+    end_time = datetime.combine(to_date, to_time)
+    dur = timedelta(hours=dur.hour, minutes=dur.minute)
+
+    def calc_intermediate_time(start_time, end_time, dur):
+        st = start_time
+        while st < end_time:
+            yield st,st+dur
+            st = st + dur
+
+    imagelist = []
+    for t_start, t_end in calc_intermediate_time(start_time, end_time, dur):
+        for img in ImageUpload.objects.filter(imgDate__in=[t_start.date(), t_end.date()], 
+                    imgTime__in =[t_start.time(), t_end.time()]):
+            imagelist.append(img)
+        
+    pathimage = [path.upload for path in imagelist]
+    outimage = interval(pathimage, Fps, float(Res))
+    return outimage
+    #videopath = videopath.replace(media_path, '/media')
 
 def index(request):
     return HttpResponse("Hello, world. You're at the GrazerFeed index.")
 
 def option(request):
     if request.method == 'POST':
+
         form = OptionsPage(request.POST)
         if form.is_valid():
             from_time = form.cleaned_data['fromtime']
@@ -23,21 +51,25 @@ def option(request):
             to_date = form.cleaned_data['todate']
             Res = form.cleaned_data['res']
             Fps = form.cleaned_data['fps']
+            dur = form.cleaned_data['duration']
             day = to_date - from_date
-            pathlist = VideoUpload.objects.filter(uploadDate__range=[from_date, to_date], resfield = Res, fpsfield = Fps)
             media_path = os.path.join(settings.BASE_DIR, 'media') 
-            pathlist = [os.path.join(media_path, str(path.uploadDate),'videos' ,path.uploadPath) for path in pathlist]
-            print(pathlist)
-            #pathlist.append(pathlist.uploadpath)
-            sec = calculate(day.days, from_time.hour, from_time.minute, to_time.hour, to_time.minute, int(Fps))
-            #videopath = settings.MEDIA_URL + str(from_date) + '/videos/video_' + str(Res) + '_' + str(Fps) + '.webm'
-            outvideo = Demuxer(pathlist, Res, Fps)
-            videopath = outvideo.multipleDemuxInput()
-            videopath = videopath.replace(media_path, '/media')
-            return render(request, 'GrazerFeed/VideoPage.html', context={'videopath' : videopath , 'start' : sec[0], 'end' : sec[1]})
+            if (dur.minute == 30):
+                pathlist = []
+                pathlist = VideoUpload.objects.filter(uploadDate__range=[from_date, to_date], resfield = Res, fpsfield = Fps)     
+                pathlist = [os.path.join(settings.MEDIA_ROOT, str(path.uploadDate),'videos' ,path.uploadPath) for path in pathlist]
+                #pathlist.append(pathlist.uploadpath)
+                sec = calculate(day.days, from_time.hour, from_time.minute, to_time.hour, to_time.minute, Fps)
+                outvideo = Demuxer(pathlist, Res, Fps)
+                videopath = outvideo.multipleDemuxInput()
+            else:
+                sec = (0, 1000000)
+                videopath = sendimage(from_date, to_date, from_time, to_time, dur, float(Fps), Res)
+                
+            finalpath = videopath.replace(media_path, '/media')
+            return render(request, 'GrazerFeed/VideoPage.html', context={'videopath' : finalpath , 'start' : sec[0], 'end' : sec[1]})
         else:
-            
-            print('form is not valid')
+            print("form error")
             print(form.errors)
             return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()}, {'error_message' : 'Invalid field values'})
     else:
@@ -45,21 +77,13 @@ def option(request):
 
 def loginuser(request):
     if request.method == 'POST':
-        print('In get')
         form = LoginPage(request.POST)
         if form.is_valid():
-            print('In is_valid()')
             username = form.cleaned_data['Username']
-            print('Username = ', username)
             password = form.cleaned_data['Password']
-            print('Password = ', password)
             user = authenticate(username=username, password=password)
-            print('Authenticated')
-            if user is not None:
-                print('not none')
-                if user.is_active:
-                    print('active')
-                    login(request, user)
-                    return render(request, 'GrazerFeed/OptionsPage.html', { 'form': OptionsPage()})
+            if user is not None and user.is_active:
+                login(request, user)
+                return redirect('OptionsPage')
     else:
         return render(request, 'GrazerFeed/LoginPage.html', { 'form': LoginPage()})        
